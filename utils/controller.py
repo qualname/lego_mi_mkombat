15461145b5fc0fbebe92995932778b19bc9f9088
qualname <mhegyi@pm.me>
2019-05-14 16:39:48 +0100

    joystick input processing

--- utils/controller.py
+++ utils/controller.py
@@ -16,8 +16,57 @@ def init_joystick():
         return None
 
 
+def _get_movement_state(joystick):
+    """ Returns movement-related `x` and `y` states.
+    Prefers 4 arrow buttons over thumbstick. """
+
+    x, y = joystick.get_hat(0)
+    if x == 0:
+        x = joystick.get_axis(0)
+    if y == 0:
+        y = joystick.get_axis(1) * -1
+
+    return x, y
+
+
 def _joy_state_to_action_space(joystick):
-    pass
+    """ Maps joystick buttons to action values. """
+
+    button_to_action_map = {
+        0: HIGH_KICK,  # △
+        1: PUNCH,      # ◯
+        2: LOW_KICK,   # ✕
+        3: BLOCK,      # □
+    }
+
+    pushed_keys = [0] * 12
+
+    for button, action in button_to_action_map.items():
+        pushed_keys[action] = joystick.get_button(button)
+
+    x, y = _get_movement_state(joystick)
+
+    if x == -1:
+        pushed_keys[MOVE_LEFT] = 1
+        pushed_keys[MOVE_RIGHT] = 0
+    elif x == 1:
+        pushed_keys[MOVE_LEFT] = 0
+        pushed_keys[MOVE_RIGHT] = 1
+    else:
+        pushed_keys[MOVE_LEFT] = 0
+        pushed_keys[MOVE_RIGHT] = 0
+
+    if y == -1:
+        pushed_keys[JUMP] = 0
+        pushed_keys[SQUAT] = 1
+    elif y == 1:
+        pushed_keys[JUMP] = 1
+        pushed_keys[SQUAT] = 0
+    else:
+        pushed_keys[JUMP] = 0
+        pushed_keys[SQUAT] = 0
+
+    return pushed_keys
 
 
 def _keyboard_state_to_action_space():
