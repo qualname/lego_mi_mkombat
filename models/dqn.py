7ac0ca13f1f0eab401a71eeacd8c0614dccb1178
qualname <mhegyi@pm.me>
2019-05-27 18:19:49 +0100

    added sample conv layers

--- models/dqn.py
+++ models/dqn.py
@@ -1,5 +1,9 @@
 import collections
+import math
 import random
+
+import torch
+import torch.nn.functional as F
 
 
 class ReplayMemory:
@@ -15,3 +19,36 @@ class ReplayMemory:
 
     def __len__(self):
         return len(self.memory)
+
+
+class QNN(torch.nn.Module):
+    def __init__(self, input_height, input_width, input_frames, outputs):
+        super().__init__()
+
+        inp_channels = input_frames * 3  # RGB images
+
+        self.conv1 = torch.nn.Conv2d(inp_channels, 16, kernel_size=7, stride=2)
+        self.bn1 = torch.nn.BatchNorm2d(16)
+        self.conv2 = torch.nn.Conv2d(16, 32, kernel_size=7, stride=2)
+        self.bn2 = torch.nn.BatchNorm2d(32)
+        self.conv3 = torch.nn.Conv2d(32, 32, kernel_size=7, stride=2)
+        self.bn3 = torch.nn.BatchNorm2d(32)
+
+        def conv_output_size(size, kernel_size=7, stride=2):
+            return math.ceil((size - kernel_size + 1) / stride)
+
+        h = conv_output_size(conv_output_size(conv_output_size(input_height)))
+        w = conv_output_size(conv_output_size(conv_output_size(input_width)))
+
+        self.fc4 = torch.nn.Linear(h * w * 32, 512)
+        self.head = torch.nn.Linear(512, outputs)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.fc4(x.view(x.size(0), -1)))
+        return self.head(x)
+
+    def sample_action(self, state, act_space, epsilon):
+        pass
