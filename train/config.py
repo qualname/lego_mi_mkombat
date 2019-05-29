47cbc423fa21072120f9a622cadc7b4932067c8e
qualname <mhegyi@pm.me>
2019-05-29 20:37:39 +0100

    init neural network

--- train/config.py
+++ train/config.py
@@ -3,3 +3,7 @@ from pathlib import Path
 ENV_NAME = 'MortalKombatII-Genesis'
 STATE_PATH = Path('../states/2players_level1_SubZeroVsJax.state')
 SCENARIO_PATH = Path('scenario.json')
+
+BUFFER_LIMIT = 10_000
+BATCH_SIZE = 128
+LEARNING_RATE = 0.001
--- train/train_dqn.py
+++ train/train_dqn.py
@@ -6,6 +6,7 @@ if __name__ == '__main__' and __package__ is None:
     sys.path.append(os.path.dirname(sys.path[0]))
 
 import config
+from models import dqn
 from utils import move_lists
 
 if __name__ == '__main__' and __package__ is None:
@@ -16,6 +17,8 @@ if not config.STATE_PATH.is_file():
     raise ValueError(f'Invalid state path ({config.STATE_PATH})!')
 if not config.SCENARIO_PATH.is_file():
     raise ValueError(f'Invalid scenario path ({config.SCENARIO_PATH})!')
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def get_state_info(state_path):
@@ -28,6 +31,17 @@ def get_state_info(state_path):
     left, right = characters.split('Vs')
 
     return player_cnt, left, right
+
+
+def init(init_observation, action_space):
+    h, w, *_ = init_observation.shape
+
+    qnn = dqn.QNN(h, w, input_frames=1, outputs=action_space.n).to(device)
+
+    memory = dqn.ReplayMemory(max_len=config.BUFFER_LIMIT, batch_size=config.BATCH_SIZE)
+    optimizer = torch.optim.Adam(qnn.parameters(), lr=config.LEARNING_RATE)
+
+    return qnn, memory, optimizer
 
 
 def main():
@@ -44,5 +58,7 @@ def main():
 
     left_action_space = move_lists.ActionSpace(left_char, env.action_space)
     # right_action_space = move_lists.ActionSpace(right_char, env.action_space)
+
+    qnn, memory, optimizer = init(env.get_screen(), left_action_space)
 
     env.close()
